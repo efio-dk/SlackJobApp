4ae080289e615eba6b509b6f8cde90e959741e02
Elitsa <elit0451@edu.eal.dk>
2020-03-05 16:36:48 +0100

    Adding functionality to request opportunity statuses from an endpoint

--- SlackJobPosterReceiver/src/SlackJobPosterReceiver/API/ClosePoster.cs
+++ SlackJobPosterReceiver/src/SlackJobPosterReceiver/API/ClosePoster.cs
@@ -25,19 +25,33 @@ namespace SlackJobPosterReceiver.API
             return await response.Content.ReadAsJsonAsync<JObject>();
         }
 
-        public async Task<JObject> PostOpportunity(string msgHeader, string leadId)
+        public async Task<JObject> PostOpportunity(string msgHeader, string leadId, string opportunityStatusName)
         {
+            string statusId = await GetStatusId(opportunityStatusName);
+
             JObject opportunityObj = new JObject
             {
                 ["note"] = msgHeader,
                 ["lead_id"] = leadId,
-                ["status_id"] = "stat_1FXxBFpJT4gWhs8zdMezZuNNT0VPbkjiIL0tLupmK4Q",
                 ["confidence"] = 0
             };
+
+            if (string.IsNullOrEmpty(statusId))
+                opportunityObj.Add("status_id", statusId);
 
             HttpResponseMessage response = await _client.PostAsJsonAsync("https://api.close.com/api/v1/opportunity/", opportunityObj, GlobalVars.CLOSE_TOKEN);
 
             return await response.Content.ReadAsJsonAsync<JObject>();
+        }
+
+        public async Task<string> GetStatusId(string statusName)
+        {
+            HttpResponseMessage response = await _client.GetAsJsonAsync("https://api.close.com/api/v1/status/opportunity/");
+            JObject responseJObj = await response.Content.ReadAsJsonAsync<JObject>();
+
+            string statusId = responseJObj.SelectToken($"$..data[?(@.label=={statusName})].id").Value<string>();
+
+            return statusId;
         }
     }
 }
--- SlackJobPosterReceiver/src/SlackJobPosterReceiver/API/HttpClientExtensions.cs
+++ SlackJobPosterReceiver/src/SlackJobPosterReceiver/API/HttpClientExtensions.cs
@@ -32,5 +32,18 @@ namespace SlackJobPosterReceiver.API
 
             return JsonConvert.DeserializeObject<T>(dataAsString);
         }
+
+        public static Task<HttpResponseMessage> GetAsJsonAsync(
+            this HttpClient httpClient, string url, string apiKey = null)
+        {
+            if (!(apiKey is null))
+            {
+                //add Basic Auth
+                var authToken = Encoding.ASCII.GetBytes($"{apiKey}:");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(authToken));
+            }
+
+            return httpClient.GetAsync(url);
+        }
     }
 }
