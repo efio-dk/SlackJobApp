eff7a281e63b5baa778067a41d4a244bd1e64e94
Elitsa <elit0451@edu.eal.dk>
2020-03-05 12:21:39 +0100

    Updating the production terraform

--- terraform/production/terraform_prod.tf
+++ terraform/production/terraform_prod.tf
@@ -3,10 +3,25 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-# Data resources (SQS)
+
+# Data resources (SQS, SSM)
 data "aws_sqs_queue" "prod-processed-job-post-queue" {
   name = "prod_ProcessedJobPosts"
 }
+
+
+data "aws_ssm_parameter" "slackleads-table" {
+  name = "TABLE_SLACK_LEADS" # our SSM parameter's name
+}
+
+data "aws_ssm_parameter" "slack-token" {
+  name = "SLACK_TOKEN" # our SSM parameter's name
+}
+
+data "aws_ssm_parameter" "close-token" {
+  name = "CLOSE_TOKEN" # our SSM parameter's name
+}
+
 
 # Lambda
 resource "aws_lambda_function" "prod-SlackJobPoster-lambda" {
@@ -14,8 +29,8 @@ resource "aws_lambda_function" "prod-SlackJobPoster-lambda" {
   handler          = "SlackJobPoster::SlackJobPoster.Function::FunctionHandler"
   runtime          = "dotnetcore2.1"
   role             = "arn:aws:iam::833191605868:role/SlackJobPosterRole"
-  filename         = "../../src/SlackJobPoster/bin/Release/netcoreapp2.1/SlackJobPoster.zip"
-  source_code_hash = filebase64sha256("../../src/SlackJobPoster/bin/Release/netcoreapp2.1/SlackJobPoster.zip")
+  filename         = "../../SlackJobPoster/src/SlackJobPoster/bin/Release/netcoreapp2.1/SlackJobPoster.zip"
+  source_code_hash = filebase64sha256("../../SlackJobPoster/src/SlackJobPoster/bin/Release/netcoreapp2.1/SlackJobPoster.zip")
   timeout          = 10
   memory_size      = 256
 
@@ -28,4 +43,81 @@ resource "aws_lambda_function" "prod-SlackJobPoster-lambda" {
 resource "aws_lambda_event_source_mapping" "prod-incoming-sqs" {
   event_source_arn = data.aws_sqs_queue.prod-processed-job-post-queue.arn
   function_name    = aws_lambda_function.prod-SlackJobPoster-lambda.arn
+}
+
+resource "aws_lambda_function" "prod-SlackJobPosterReceiver-lambda" {
+  function_name    = "prod-SlackJobPosterReceiver"
+  handler          = "SlackJobPosterReceiver::SlackJobPosterReceiver.Function::Get"
+  runtime          = "dotnetcore2.1"
+  role             = "arn:aws:iam::833191605868:role/DeleteThisRole"
+  filename         = "../../SlackJobPosterReceiver/src/SlackJobPosterReceiver/bin/Release/netcoreapp2.1/SlackJobPosterReceiver.zip"
+  source_code_hash = filebase64sha256("../../SlackJobPosterReceiver/src/SlackJobPosterReceiver/bin/Release/netcoreapp2.1/SlackJobPosterReceiver.zip")
+  timeout          = 10
+  memory_size      = 256
+
+  tags = {
+    Name        = "prod-SlackJobPosterReceiver"
+    Environment = "production"
+  }
+
+  environment {
+    variables = {
+      AWS_TABLE_SLACK_LEADS = data.aws_ssm_parameter.slackleads-table.value
+      SLACK_TOKEN           = data.aws_ssm_parameter.slack-token.value
+      CLOSE_TOKEN           = data.aws_ssm_parameter.close-token.value
+    }
+  }
+}
+
+
+# API Gateway
+resource "aws_api_gateway_rest_api" "slack-app-api" {
+  name = "slackAppApi"
+}
+
+resource "aws_api_gateway_resource" "slack-receiver-resource" {
+  path_part   = "slackReceiver"
+  parent_id   = aws_api_gateway_rest_api.slack-app-api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.slack-app-api.id
+}
+
+resource "aws_api_gateway_method" "slack-receiver-any" {
+  rest_api_id   = aws_api_gateway_rest_api.slack-app-api.id
+  resource_id   = aws_api_gateway_resource.slack-receiver-resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.slack-app-api.id
+  resource_id             = aws_api_gateway_resource.slack-receiver-resource.id
+  http_method             = aws_api_gateway_method.slack-receiver-any.http_method
+  integration_http_method = "ANY"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.prod-SlackJobPosterReceiver-lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "slack-receiver-api-deployment" {
+  depends_on = [aws_api_gateway_integration.integration]
+
+  rest_api_id = aws_api_gateway_rest_api.slack-app-api.id
+  stage_name  = "production"
+}
+
+# Dynamo DB
+resource "aws_dynamodb_table" "prod-slack-leads-table" {
+  name           = "prod_SlackLeads"
+  hash_key       = "message_ts"
+  read_capacity  = 5
+  write_capacity = 5
+
+  attribute {
+    name = "message_ts"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "prod-slack-leads-table"
+    Environment = "production"
+  }
 }
